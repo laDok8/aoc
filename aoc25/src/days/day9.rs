3cc44e32aa4a225dc5b15b1b8784ff83f90461cc
Ladislav <ldokoupi@redhat.com>
2025-12-30 15:10:34 +0100

    25 d9 part1

--- aoc25/src/days/day9.rs
+++ aoc25/src/days/day9.rs
@@ -1,5 +1,25 @@
-fn part1(_input: &str) {
-    todo!("Part 1 not implemented yet");
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn rect_area(&self, p: &Point) -> i64 {
+        (p.x - self.x + 1 ).abs() as i64 * (p.y - self.y + 1).abs() as i64
+    }
+}
+
+fn part1(points: &[Point]) {
+    let mut max_area = 0;
+
+    for i in 0..points.len() {
+        for j in i + 1..points.len() {
+            max_area = max_area.max(points[i].rect_area(&points[j]));
+        }
+    }
+
+    println!("Part 1: {max_area}");
 }
 
 fn part2(_input: &str) {
@@ -8,6 +28,18 @@ fn part2(_input: &str) {
 
 pub fn day9() {
     let input = include_str!("../inputs/day9.txt").trim();
-    part1(input);
+
+    let points: Vec<Point> = input
+        .lines()
+        .map(|line| {
+            let (x, y) = line.split_once(',').unwrap();
+            Point {
+                x: x.parse().unwrap(),
+                y: y.parse().unwrap(),
+            }
+        })
+        .collect();
+
+    part1(&points);
     part2(input);
 }
