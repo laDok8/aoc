46c5a96d11c22105c760b23b86d31b70fe554b1b
Ladislav <ldokoupi@redhat.com>
2026-01-02 10:12:22 +0100

    25 day 12 jolly christmas

--- aoc25/src/days/day12.rs
+++ aoc25/src/days/day12.rs
@@ -1,13 +1,33 @@
-fn part1(_input: &str) {
-    todo!("Part 1 not implemented yet");
-}
+const BOX_SIZE: i32 = 3;
 
-fn part2(_input: &str) {
-    todo!("Part 2 not implemented yet");
+fn part1(input: &str) {
+    let fitting_count = input
+        .lines()
+        .filter(|line| {
+            let (dimensions, present_ids) = line.split_once(':').unwrap();
+
+            let capacity = dimensions
+                .split('x')
+                .map(|dim| dim.trim().parse::<i32>().unwrap() / BOX_SIZE)
+                .product::<i32>();
+
+            let total_presents: i32 = present_ids
+                .split_whitespace()
+                .map(|id| id.parse::<i32>().unwrap())
+                .sum();
+
+            total_presents <= capacity
+        })
+        .count();
+
+    println!("Part 1: {fitting_count}");
 }
 
 pub fn day12() {
+    // First puzzle not working on example input :/
     let input = include_str!("../inputs/day12.txt").trim();
-    part1(input);
-    part2(input);
+
+    let regions = input.split("\n\n").last().unwrap_or_default();
+
+    part1(regions);
 }
